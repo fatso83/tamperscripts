902cd1f421f674de1e99a04652e3351dd17b0f56
Carl-Erik Kopseng <carlerik@gmail.com>
2018-04-18 19:41:51 +0200

    Match on whole domain

--- meny-perf.js
+++ meny-perf.js
@@ -4,41 +4,40 @@
 // @version      0.1
 // @description  Improves the performance (reduces cpu usage) on meny.no
 // @author       Carl-Erik Kopseng
-// @match        https://meny.no
+// @match        https://meny.no/*
 // @grant        none
 // ==/UserScript==
 
 (function script() {
-    // the svg with id 'spinner' in the svg definitions causes a cpu
-    // load of approximately 24% constantly when monitored
-    // using Chrome Process Explorer (sorted on cpu usage), Shift-Esc
+  // the svg with id 'spinner' in the svg definitions causes a cpu
+  // load of approximately 24% constantly when monitored
+  // using Chrome Process Explorer (sorted on cpu usage), Shift-Esc
 
+  // some experiments on why this happens
 
-    // some experiments on why this happens
+  const spinner = document.getElementById("spinner");
 
-    const spinner = document.getElementById('spinner');
+  if (!spinner) {
+    setTimeout(script, 200);
+    return;
+  }
 
-    if(!spinner) {
-        setTimeout(script, 200);
-        return;
-    }
+  const defs = spinner.parentElement;
+  const svg = defs.parentElement;
 
-    const defs = spinner.parentElement;
-    const svg = defs.parentElement;
+  // to remove cpu use of approx 24%, just remove the element ...
+  const removeSpinner = () => defs.removeChild(spinner);
 
-    // to remove cpu use of approx 24%, just remove the element ...
-    const removeSpinner = () => defs.removeChild(spinner);
+  // to re-add the cpu spike:
+  const addSpinner = () => defs.appendChild(spinner);
 
-    // to re-add the cpu spike:
-    const addSpinner = () => defs.appendChild(spinner);
+  // to adjust cpu usage from 24% to 12%, set the svg element to display:none
+  // things still work in Chrome, but this might cause bugs in other browsers
+  // as this removes the element from the DOM, thus potentially making it
+  // impossible to find when referencing it
+  const displayNone = () => (svg.style.display = "none");
+  const displayInitial = () => (svg.style.display = "initial");
 
-    // to adjust cpu usage from 24% to 12%, set the svg element to display:none
-    // things still work in Chrome, but this might cause bugs in other browsers
-    // as this removes the element from the DOM, thus potentially making it
-    // impossible to find when referencing it
-    const displayNone = () => svg.style.display = 'none';
-    const displayInitial = () => svg.style.display = 'initial';
-
-    console.log('Removed cpu hog spinner');
-    removeSpinner();
+  console.log("Removed cpu hog spinner");
+  removeSpinner();
 })();
